e71f194db09c3218d31bb2edb9cbc74cab5d0965
amartin102 <martinezb19@gmail.com>
2025-08-26 01:25:49 -0500

    entregable prueba tecnica

--- Application/Interface/IPropertyService.cs
+++ Application/Interface/IPropertyService.cs
@@ -17,13 +17,6 @@ namespace Application.Interface
         Task<PropertyDetailDto?> GetByIdAsync(string id, CancellationToken ct);
 
         Task<PaginationResult<PropertyItemDto>> GetAllPropertiesByFilter(PropertyFilterDto filter, CancellationToken ct);
-
-        //Task<IEnumerable<PropertyListItemDto>> GetPropertiesAsync(PropertyFilterDto filter, CancellationToken ct);
-
-        // UPDATE
-        //Task<bool> UpdatePropertyAsync(PropertyItemDto dto, CancellationToken ct);
-
-        // DELETE
-        //Task<bool> DeletePropertyAsync(string id, CancellationToken ct);
+       
     }
 }
